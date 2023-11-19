5c273a260c20d26e4fc6eb8ae1a69cc1c9b38afb
Amadeusz Iwanowski <a.iwanowski@zoovu.com>
2023-11-19 10:12:26 +0100

    Working version finally

--- zad1_rozpoznawanie_liczb/ai.js
+++ zad1_rozpoznawanie_liczb/ai.js
@@ -1,25 +1,22 @@
-console.log("hello world")
-
-const pixels = [];
+const displayedPixels = [];
 const IMAGE_LENGTH = 28 * 28;
 const VECTOR_LENGTH = IMAGE_LENGTH + 1;
 const HOW_MANY_NUMBERS = 10;
 
 document.querySelectorAll(".cell").forEach((element, index) => {
-    pixels.push(0);
-    element.setAttribute("value", pixels[index]);
+    displayedPixels.push(0);
+    element.setAttribute("value", displayedPixels[index]);
     element.addEventListener("click", (event) => {
-        pixels[index] = 1 - pixels[index];
-        event.target.setAttribute("value", pixels[index]);
+        displayedPixels[index] = 1 - displayedPixels[index];
+        event.target.setAttribute("value", displayedPixels[index]);
     });
 });
 
-function verify() {
-    debugger;
+function verifyAll() {
     let correctly = 0;
-    const allImages = trainingImages.length;
-    for (let i = 0; i < trainingImages.length; i++) {
-        const correctNumber = getCurrentCorrectNumber(i);
+    const allImages = testingImages.length;
+    for (let i = 0; i < testingImages.length; i++) {
+        const correctNumber = testingLabels[i];
         const guessedNumber = guessNumber(i);
         if (correctNumber === guessedNumber) {
             correctly++;
@@ -29,8 +26,26 @@ function verify() {
     console.log(`Final results: ${correctly} / ${allImages}: ${correctly / allImages * 100}%`)
 }
 
+function verifyOne() {
+    let correctly = 0;
+    const allImages = testingImages.length;
+    for (let i = 0; i < testingImages.length; i++) {
+        const correctNumber = testingLabels[i];
+        const guessedPrediction = guessNumberHelper(currentNumber, i);
+
+        const isOk1 = guessedPrediction > 0 ? 1 : -1;
+        const isOk2 = correctNumber === currentNumber ? 1 : -1;
+
+        if (isOk1 === isOk2) {
+            correctly++;
+        }
+    }
+
+    console.log(`Final results: ${correctly} / ${allImages}: ${correctly / allImages * 100}%`)
+}
+
 function guessNumberHelper(number, trainingIndex) {
-    const currentPixels = trainingImages[trainingIndex];
+    const currentPixels = testingImages[trainingIndex];
     const predictionNumber = math.multiply(math.transpose(weights[number]), currentPixels);
     return predictionNumber;
 }
@@ -60,11 +75,14 @@ for (let j = 0; j < HOW_MANY_NUMBERS; j++) {
 
 let currentEpoch = 0;
 
+let learningConstant = 10000;
+
 function learnHelper(index) {
+    currentNumber = index;
     currentEpoch = 0;
     while (true) {
         let correctlyGuessed = 0;
-        for (let i = 0; i < 4 * 60000; i++) {
+        for (let i = 0; i < 1 * 60000; i++) {
             const guessIndex = Math.floor(Math.random() * 60000);
             if (isNaN(guessIndex)) {
                 console.log("prediction is nan")
@@ -96,7 +114,7 @@ function learnHelper(index) {
             if (isNaN(error)) {
                 console.log("prediction is nan")
             }
-            const multiplier = error / 1000000;
+            const multiplier = error / learningConstant;
             if (isNaN(multiplier)) {
                 console.log("prediction is nan")
             }
@@ -113,32 +131,39 @@ function learnHelper(index) {
                 correctlyGuessed++;
             }
 
-            if (i % 4 * 10000 === 0) {
+            if (i % (6 * 10000) === 0) {
                 console.log(`How many : ${correctlyGuessed} / ${i}`)
             }
         }
-        console.log("correctlyGuessed", correctlyGuessed);
+        console.log(`Current number: ${currentNumber} . Correctly guessed ${correctlyGuessed}` );
         currentEpoch++;
-        if (currentEpoch > 5) {
+        if (currentEpoch > 0) {
             console.log("limit przekroczony - kończę naukę")
             break;
         }
-        if (correctlyGuessed >= 119900) {
+        if (correctlyGuessed >= 4 * 60000 - 1000) {
             console.log("dobry wynik - koniec nauki");
             break;
         }
     }
 }
 
-function learn() {
+let currentNumber = 0;
+
+function learnAll() {
     for (let i = 0; i < HOW_MANY_NUMBERS; i++) {
         learnHelper(i);
     }
 }
+
+function learn() {
+    learnHelper(currentNumber);
+}
+
 window.show = (newPixels) => {
     document.querySelectorAll(".cell").forEach((element, index) => {
         element.setAttribute("value", newPixels[index]);
-        pixels[index] = newPixels[index];
+        displayedPixels[index] = newPixels[index];
     });
 }
 
@@ -229,3 +254,76 @@ function parseLabels(contents) {
 
 document.getElementById('file-input2')
     .addEventListener('change', readSingleFile2, false);
+
+
+function readSingleFile3(e) {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        parseImages2(contents);
+    };
+    reader.readAsArrayBuffer(file);
+}
+
+var testingImages = []
+
+function parseImages2(contents) {
+    testingImages = [];
+    const view = new Uint8Array(contents);
+    let start = 16;
+    let i = start;
+    while (true) {
+        if (i >= view.length) {
+            break
+        }
+        const currentImage = []
+        for (let j = 0; j < IMAGE_LENGTH; j++) {
+            currentImage.push(view.at(i) / 255);
+            i++;
+        }
+        currentImage.push(1);
+        testingImages.push(math.matrix(currentImage));
+        console.log(`image done ${Math.floor(i / IMAGE_LENGTH)}`)
+    }
+
+
+    window.console.log(testingImages[0])
+    show(testingImages[0]._data)
+}
+
+document.getElementById('file3')
+    .addEventListener('change', readSingleFile3, false);
+
+
+function readSingleFile4(e) {
+    var file = e.target.files[0];
+    if (!file) {
+        return;
+    }
+    var reader = new FileReader();
+    reader.onload = function(e) {
+        var contents = e.target.result;
+        parseLabels2(contents);
+    };
+    reader.readAsArrayBuffer(file);
+}
+
+var testingLabels = []
+
+function parseLabels2(contents) {
+    testingLabels = [];
+    const view = new Uint8Array(contents);
+    let start = 8;
+    for (let i = start; i < view.length; i++) {
+        testingLabels.push(view.at(i));
+    }
+
+    window.console.log(testingLabels)
+}
+
+document.getElementById('file4')
+    .addEventListener('change', readSingleFile4, false);
